e38410f663017face0216311b3366e613be99940
MAMADOU LAMARANA DIALLO <lamarana55@gmail.com>
2023-05-31 21:12:16 +0000

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:   "http://localhost:8081/api/", // 'https://ws.proril-guinee.com/api/',
+  apiUrl:  'https://ws.proril-guinee.com/api/', // "http://localhost:8081/api/", 
   userConnectedKey: 'key_of_user_bbgOTVpdfhdgfhgeu'
 };
